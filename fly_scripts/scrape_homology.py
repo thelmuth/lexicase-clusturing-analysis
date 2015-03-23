abb29a38a1e575d33b0bc03875c75400d589fd53
Nic McPhee <mcphee@morris.umn.edu>
2015-03-23 18:26:16 -0400

    Fixed some comments.
    There were some comments that were wrong, and I added some comments in a few places to try to help explain what's happening.
    
    The huge pile of regex matches and if-tests is, well, much of the ugly and really ought to be cleaned up.

--- fly_scripts/scrape_homology.py
+++ fly_scripts/scrape_homology.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 # Takes a directory name as a single command line argument, and scrapes the
-# homology data out of all the log*.txt files in that directory, putting the
-# results in the name of the directory with "_homology.csv" appended.
+# homology data out of all the log*.txt files in that directory, writing
+# the quartile data out to standard output.
 
 import sys
 import glob
@@ -13,19 +13,21 @@ def print_row(run, generation, quartile, value):
 
 directory_to_scrape = sys.argv[1]
 
+# Print the header line for the output CSV
 print("Run,Generation,Quartile,Homology")
+
 for file in glob.glob(directory_to_scrape + "/log*.txt"):
   with open(file) as f:
+      # Extract the run number from the file name so I can include it
+      # in the output in the Run column.
       run = re.match(".*log(\d+).txt", file).groups()[0]
       for line in f:
-          # print("<", line, ">")
+          # This whole chain of matches is super ugly and could almost
+          # certainly be cleaned up a ton.
           is_gen_line = re.match("Processing generation: (\d+)", line.strip())
-          # print(is_gen_line)
           if is_gen_line:
               generation = is_gen_line.groups()[0]
-          # First quartile:  0.93203884
           is_first_quartile = re.match("First quartile(\s+\(sample 1\))?:\s+(\d+\.\d+)", line.strip())
-          # print(is_first_quartile)
           if is_first_quartile:
               first_quartile = is_first_quartile.groups()[1]
               print_row(run, generation, "0.25", first_quartile)
